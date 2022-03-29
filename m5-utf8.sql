ee51566334f818bdeb1eb0cdffa492780dd641c1
nrr <nrr@warsong.me>
2022-03-29 23:51:46 +0700

    m5 changes

--- m5-utf8.sql
+++ m5-utf8.sql
@@ -9,13 +9,13 @@ SET search_path TO public;
 --Пронумеруйте все платежи от 1 до N по дате
 
 select *,
-	row_number () over (order by payment_date) as "ex 1-1"
+	row_number () over (order by payment_date)
 from payment p;
 
 --Пронумеруйте платежи для каждого покупателя, сортировка платежей должна быть по дате
 
 select *,
-	row_number () over (partition by customer_id order by payment_date) as "ex 1-2"
+	row_number () over (partition by customer_id order by payment_date)
 from payment p;
 
 --Посчитайте нарастающим итогом сумму всех платежей для каждого покупателя, сортировка должна 
@@ -29,35 +29,50 @@ from payment p;
 --так, чтобы платежи с одинаковым значением имели одинаковое значение номера.
 
 select *,
-row_number ()
+dense_rank  () over (partition by customer_id order by amount desc)
 from payment p;
 
 --Можно составить на каждый пункт отдельный SQL-запрос, а можно объединить все колонки в одном запросе.
 
-
+select *,
+	row_number () over (order by payment_date) as "ex 1-1",
+	row_number () over (partition by customer_id order by payment_date) as "ex 1-2",
+	sum(amount) over (partition by customer_id order by payment_date,amount rows between unbounded preceding and current row) as "ex 1-3",
+	dense_rank () over (partition by customer_id order by amount desc) as "ex 1-4"
+from payment p 
+order by customer_id;
 
 
 --ЗАДАНИЕ №2
 --С помощью оконной функции выведите для каждого покупателя стоимость платежа и стоимость 
 --платежа из предыдущей строки со значением по умолчанию 0.0 с сортировкой по дате.
 
-
-
-
+select *,
+	lag(amount,1,0.00) over (partition by customer_id order by payment_date)
+from payment p;
 
 --ЗАДАНИЕ №3
 --С помощью оконной функции определите, на сколько каждый следующий платеж покупателя больше или меньше текущего.
 
-
-
-
+select *,
+lag(amount) over (partition by customer_id order by payment_date) - amount as "price difference"
+from payment p;
 
 --ЗАДАНИЕ №4
 --С помощью оконной функции для каждого покупателя выведите данные о его последней оплате аренды.
 
-
-
-
+with cte as(
+select *,
+	NTILE(payment_id) over (partition by customer_id order by payment_date desc) as ntl
+from payment)
+select 
+	customer_id ,
+	payment_id ,
+	rental_id ,
+	amount ,
+	payment_date  
+from cte 
+where ntl=1;
 
 --======== ДОПОЛНИТЕЛЬНАЯ ЧАСТЬ ==============
 
