e5c10c4c2f2adf0225eed696c41cc3d172b09fbc
nrr <nrr@warsong.me>
2022-03-30 01:09:56 +0700

    m5 changes

--- m5-utf8.sql
+++ m5-utf8.sql
@@ -8,71 +8,70 @@ SET search_path TO public;
 --Сделайте запрос к таблице payment и с помощью оконных функций добавьте вычисляемые колонки согласно условиям:
 --Пронумеруйте все платежи от 1 до N по дате
 
-select *,
+select customer_id, payment_id , payment_date ,
 	row_number () over (order by payment_date)
-from payment p;
+from payment p ;
 
 --Пронумеруйте платежи для каждого покупателя, сортировка платежей должна быть по дате
 
-select *,
+select customer_id, payment_id , payment_date , 
 	row_number () over (partition by customer_id order by payment_date)
 from payment p;
 
 --Посчитайте нарастающим итогом сумму всех платежей для каждого покупателя, сортировка должна 
 --быть сперва по дате платежа, а затем по сумме платежа от наименьшей к большей
 
-select *,
-	sum(amount) over (partition by customer_id order by payment_date, amount rows between unbounded preceding and current row) as "ex 1-3"
+select customer_id, payment_id , payment_date , amount, 
+	sum(amount) over (partition by customer_id order by payment_date, amount rows between unbounded preceding and current row)
 from payment p; 
 
 --Пронумеруйте платежи для каждого покупателя по стоимости платежа от наибольших к меньшим 
 --так, чтобы платежи с одинаковым значением имели одинаковое значение номера.
 
-select *,
+select customer_id , payment_id , amount,
 dense_rank  () over (partition by customer_id order by amount desc)
 from payment p;
 
 --Можно составить на каждый пункт отдельный SQL-запрос, а можно объединить все колонки в одном запросе.
 
-select *,
-	row_number () over (order by payment_date) as "ex 1-1",
-	row_number () over (partition by customer_id order by payment_date) as "ex 1-2",
-	sum(amount) over (partition by customer_id order by payment_date,amount rows between unbounded preceding and current row) as "ex 1-3",
-	dense_rank () over (partition by customer_id order by amount desc) as "ex 1-4"
+select customer_id , payment_id , payment_date ,
+	row_number () over (order by payment_date) as "column_1",
+	row_number () over (partition by customer_id order by payment_date) as "column_2",
+	sum(amount) over (partition by customer_id order by payment_date,amount rows between unbounded preceding and current row) as "column_3",
+	dense_rank () over (partition by customer_id order by amount desc) as "column_4"
 from payment p 
-order by customer_id;
+order by customer_id ;
 
 
 --ЗАДАНИЕ №2
 --С помощью оконной функции выведите для каждого покупателя стоимость платежа и стоимость 
 --платежа из предыдущей строки со значением по умолчанию 0.0 с сортировкой по дате.
 
-select *,
-	lag(amount,1,0.00) over (partition by customer_id order by payment_date)
+select customer_id , payment_id , payment_date , amount,
+	lag(amount,1,0.00) over (partition by customer_id order by payment_date) as "last_amount"
 from payment p;
 
 --ЗАДАНИЕ №3
 --С помощью оконной функции определите, на сколько каждый следующий платеж покупателя больше или меньше текущего.
 
-select *,
-lag(amount) over (partition by customer_id order by payment_date) - amount as "price difference"
+select customer_id , payment_id , payment_date , amount ,
+amount - lag(amount,-1) over (partition by customer_id order by payment_date) as "difference"
 from payment p;
 
 --ЗАДАНИЕ №4
 --С помощью оконной функции для каждого покупателя выведите данные о его последней оплате аренды.
 
 with cte as(
-select *,
-	NTILE(payment_id) over (partition by customer_id order by payment_date desc) as ntl
+select customer_id , payment_id , payment_date , amount ,
+	NTILE(payment_id) over (partition by customer_id order by payment_date desc) as lul
 from payment)
 select 
 	customer_id ,
 	payment_id ,
-	rental_id ,
-	amount ,
-	payment_date  
+	payment_date, 
+	amount  
 from cte 
-where ntl=1;
+where lul=1;
 
 --======== ДОПОЛНИТЕЛЬНАЯ ЧАСТЬ ==============
 
